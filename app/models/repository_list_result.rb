565b6e54d775131b5b5fb90b933f006d1283ee7d
Lance Taylor <lance.taylor98@hotmail.com>
2019-01-29 11:32:17 -0700

    removes debugging tool pry line

--- app/models/repository_list_result.rb
+++ app/models/repository_list_result.rb
@@ -4,7 +4,6 @@ class RepositoryListResult
     data = GithubService.new
     repository_data = data.user_repositories
     repository_data.map do |repo_data|
-      binding.pry
       Repository.new(repo_data)
     end
   end
