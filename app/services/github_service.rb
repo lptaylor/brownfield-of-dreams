8acad8467c61ad323932fac15d9e90371dbf5233
Lance Taylor <lance.taylor98@hotmail.com>
2019-01-29 11:30:51 -0700

    gets authorization to work for repo_data in Repository_list_result model referencing the conn method within this file

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,12 +1,16 @@
 class GithubService
 
-  private
-
   def conn
-    Faraday.new(url: "https://api.github.com/user/repos") do |f|
+    Faraday.new(url: "https://api.github.com") do |f|
       f.adapter  Faraday.default_adapter
-      f.params[:key] = ENV['']
+      f.headers[:Authorization] = "token #{ENV['GIT_HUB_PLACEHOLDER_KEY']}"
     end
   end
+
+  def user_repositories
+    response = conn.get("/user/repos")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
 
 end
