01630461263e7049866ce6f106c4638c7940f35c
Lance Taylor <lance.taylor98@hotmail.com>
2019-01-29 13:54:26 -0700

    fixes call to .url to .html_url

--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Repository, type: :model do
   before(:each) do
-    @repository = Repository.new(name: "test-repo", url: "https://something")
+    @repository = Repository.new(name: "test-repo", html_url: "https://something")
 
   end
   it 'exists' do
@@ -11,6 +11,6 @@ RSpec.describe Repository, type: :model do
 
   it 'has attributes' do
     expect(@repository.name).to eq("test-repo")
-    expect(@repository.url).to eq("https://something")
+    expect(@repository.html_url).to eq("https://something")
   end
 end
