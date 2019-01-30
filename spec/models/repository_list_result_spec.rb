6b9227628eb08c9fb8dde65b23f2724c8a3e196b
Lance Taylor <lance.taylor98@hotmail.com>
2019-01-30 11:13:34 -0700

    Makes testing for RepositoryListResult now for RepositoryListResultFacade

--- spec/models/repository_list_result_spec.rb
+++ spec/models/repository_list_result_spec.rb
@@ -1,17 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe RepositoryListResult, type: :model do
+RSpec.describe RepositoryListResultFacade, type: :model do
   before(:each) do
     VCR.use_cassette('repository_list_test') do
-      @repositories = RepositoryListResult.user_repository_list
+      @user = create(:user)
+      repo_list = RepositoryListResultFacade.new(@user)
+      @repositories = repo_list.user_repository_list
     end
   end
 
   it 'exists' do
-    repositories = RepositoryListResult.new
-    expect(repositories).to be_a(RepositoryListResult)
+    repositories = RepositoryListResultFacade.new(@user)
+    expect(repositories).to be_a(RepositoryListResultFacade)
   end
 
   it 'returns a list of all repositories' do
+    expect(@repositories).to be_a(Array)
   end
 end
