cf20a6f7d6269f3a1ea74210c71c554574fb8393
Lance Taylor <lance.taylor98@hotmail.com>
2019-01-30 11:34:17 -0700

    Removes commented out user story

--- spec/features/user/user_sees_github_information_spec.rb
+++ spec/features/user/user_sees_github_information_spec.rb
@@ -33,8 +33,3 @@ describe 'non token or no repos' do
 
   end
 end
-# As a logged in user
-# When I visit /dashboard
-# Then I should see a section for "Github"
-# And under that section I should see a list
-# of 5 repositories with the name of each Repo linking to the repo on Github
