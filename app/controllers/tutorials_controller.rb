6f070ca43367676a77ef766856b0a1ec4157ea36
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-01-30 14:06:09 -0700

    fixed tutorial show page reload

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -2,7 +2,7 @@ class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
     @facade = TutorialFacade.new(tutorial, params[:video_id])
-    if @facade.current_video.id
+    if @facade.current_video
 
     else
       flash[:error] = "Sorry, This tutorial is empty"
