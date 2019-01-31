860dca50718f93e9a88300fe846e451d1b4e3951
Lance Taylor <lance.taylor98@hotmail.com>
2019-01-31 08:30:53 -0700

    adds basic test for followers to be seen in the github section of a profile

--- spec/features/user/user_sees_github_information_spec.rb
+++ spec/features/user/user_sees_github_information_spec.rb
@@ -33,3 +33,25 @@ describe 'non token or no repos' do
 
   end
 end
+
+describe 'followers' do
+  before(:each) do
+    VCR.use_cassette('repository_data') do
+      @user = create(:user)
+      followers_data = FollowersListFacade.new(@user)
+      @followers = followers_data.user_followers_list
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      visit '/dashboard'
+    end
+  end
+  it 'displays followers for an authenticated user' do
+    within '.github' do
+      all('.followers').first do
+        within "followers-#{@followers[0].id}" do
+          expect(page).to have_content(@followers[0].handle)
+          expect(page).to have_link(@followers[0].handle)
+        end
+      end
+    end
+  end
+end
