9c8c1cd352934e2d3500670d2eb464aefdea5938
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-02-02 16:14:39 -0700

    Added Mailers for account validation

---
+++ app/mailers/confiration_mailer.rb
@@ -0,0 +1,6 @@
+class ConfirationMailer < ApplicationMailer
+  def success(current_user)
+    @user = current_user
+    mail(to: @user.email, subject: "Activate Turing Tutorials Account!")
+  end
+end
