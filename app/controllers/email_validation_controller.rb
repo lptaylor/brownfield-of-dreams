0c44b1cda2b3b5102968bdaabb8ae3e5a6154157
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-02-02 17:06:02 -0700

    Added a welcome to dashboard show page

--- app/controllers/email_validation_controller.rb
+++ app/controllers/email_validation_controller.rb
@@ -2,6 +2,7 @@ class EmailValidationController < ApplicationController
   def create
     user = User.find(params[:id])
     user.status == true
+    # binding.pry
     user.save!
     redirect_to(welcome_thanks_path)
   end
