2f73064be8bb5ce43f2d4b901895f2cd501eab21
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-02-04 15:52:03 -0700

    Adding further testing and cleaning up code

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     last_name { Faker::Artist.name }
     password { Faker::Color.color_name }
     role { :default }
-    token { "token #{ENV['GIT_HUB_PLACEHOLDER_KEY']}" }
+    token { "#{ENV['GIT_HUB_PLACEHOLDER_KEY']}" }
   end
 
   factory :admin, parent: :user do
