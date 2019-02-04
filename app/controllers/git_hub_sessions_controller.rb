b308e6f0e4aea6eb9bce0860aef362e5faf6c29a
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-04 15:04:36 -0700

    updates Github Sessions controller to add uid to users during oauth

--- app/controllers/git_hub_sessions_controller.rb
+++ app/controllers/git_hub_sessions_controller.rb
@@ -1,14 +1,14 @@
 class GitHubSessionsController < ApplicationController
   def create
     @user = current_user
-    @user.update_attributes(token: auth_hash)
+    @user.update_attributes(token: auth_hash['credentials']['token'], uid: auth_hash['uid'] )
     @user.save
     redirect_to '/dashboard'
   end
 
   private
   def auth_hash
-    request.env['omniauth.auth']['credentials']['token']
+    request.env['omniauth.auth']
   end
 
 end
