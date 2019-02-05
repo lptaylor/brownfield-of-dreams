62051134f4901b4c38e397538a511b5d2116b4ec
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-02-05 15:39:11 -0700

    Working on email invitations
    Test written, and mailer is written.

---
+++ app/controllers/invite_controller.rb
@@ -0,0 +1,16 @@
+class InviteController < ApplicationController
+  def new
+  end
+
+  def create
+    @facade = InviteFacade.new(params[:github], current_user)
+    if @facade.friend_email.nil?
+      flash[:error] = "The Github user you selected doesn't have an email address associated with their account."
+      render :new
+    else
+      FriendshipMailer.friendship(@facade).deliver_now
+      flash[:notice] = "Successfully sent invite!"
+      redirect_to dashboard_path
+    end
+  end
+end
---
+++ app/facades/invite_facade.rb
@@ -0,0 +1,35 @@
+class InviteFacade
+
+  def initialize(github_user, current_user)
+    @github_user = github_user
+    @current_user = current_user
+    @_current_user_search_result
+    @_friend_search_result
+  end
+
+  def friend_name
+    friend_search_result.response_single_user[:name]
+  end
+
+  def user_name
+    current_user_search_result.response_single_user[:name]
+  end
+
+  def friend_email
+    friend_search_result.response_single_user[:email]
+  end
+
+  private
+  # def current_user_search_result
+  #   binding.pry
+  #   @_current_user_search_result ||= service(@current_user.gusername)
+  # end
+
+  def friend_search_result
+    @_friend_search_result ||= service(@github_user)
+  end
+
+  def service(github_username)
+    GithubSingleService.new(github_username, @current_user.token)
+  end
+end
