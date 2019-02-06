fb8b728038fbc90f00260db74fc91939d955ee2b
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-06 13:22:27 -0700

    adds follower_github model and retools existing follower model from a PORO to an ActiveRecord object.

---
+++ app/models/follower_github.rb
@@ -0,0 +1,8 @@
+class FollowerGithub
+  attr_reader :id, :handle, :html_url
+  def initialize(attributes)
+    @id = attributes[:id]
+    @handle = attributes[:login]
+    @html_url = attributes[:html_url]
+  end
+end
