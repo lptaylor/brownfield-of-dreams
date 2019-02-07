2b25affc824f447f4b88608e4d537071c51a8327
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-07 12:14:16 -0700

    add testing assertions for story 1

--- spec/features/admin/admin_can_create_a_tutorial_spec.rb
+++ spec/features/admin/admin_can_create_a_tutorial_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-xfeature "An admin can create" do
+feature "An admin can create" do
   scenario "can create a new tutorial" do
     admin = create(:admin)
 
@@ -8,10 +8,21 @@ xfeature "An admin can create" do
 
     visit new_admin_tutorial_path
 
-    fill_in "title", with: "Docker Container"
-    fill_in "description", with: "Kickass Container Tech."
-    fill_in "thumbnail", with: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi9p4fWjqngAhWTHDQIHXo2Bu0QjRx6BAgBEAU&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fwww.techrepublic.com%252Farticle%252Fdocker-the-smart-persons-guide%252F%26psig%3DAOvVaw25-tEYNTkPxtgJuL32okq1%26ust%3D1549611456594975&psig=AOvVaw25-tEYNTkPxtgJuL32okq1&ust=1549611456594975"
+    fill_in 'tutorial_title', with: "Docker Container"
+    fill_in "tutorial_description", with: "Kickass Container Tech."
+    fill_in "tutorial_thumbnail", with: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi9p4fWjqngAhWTHDQIHXo2Bu0QjRx6BAgBEAU&url=%2Furl%3Fsa%3Di%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttps%253A%252F%252Fwww.techrepublic.com%252Farticle%252Fdocker-the-smart-persons-guide%252F%26psig%3DAOvVaw25-tEYNTkPxtgJuL32okq1%26ust%3D1549611456594975&psig=AOvVaw25-tEYNTkPxtgJuL32okq1&ust=1549611456594975"
     click_on "Save"
+    binding.pry
     save_and_open_page
+    expect(current_path).to eq(tutorial_path(Tutorial.last.id))
+    expect(page).to have_content("Docker Container has been successfully added!")
   end
+#
+#   When I visit '/admin/tutorials/new'
+# And I fill in 'title' with a meaningful name
+# And I fill in 'description' with a some content
+# And I fill in 'thumbnail' with a valid YouTube thumbnail
+# And I click on 'Save'
+# Then I should be on '/tutorials/{NEW_TUTORIAL_ID}'
+# And I should see a flash message that says "Successfully created tutorial."
 end
