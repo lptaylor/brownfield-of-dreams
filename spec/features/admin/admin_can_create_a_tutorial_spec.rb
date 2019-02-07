b2a6214bf435f7132bae9d2ae228430a2f3c219a
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-07 15:07:44 -0700

    adds better testing to make sure all info is required and shows error message

--- spec/features/admin/admin_can_create_a_tutorial_spec.rb
+++ spec/features/admin/admin_can_create_a_tutorial_spec.rb
@@ -16,4 +16,44 @@ feature "An admin can create" do
     expect(page).to have_content("Docker Container has been successfully added!")
   end
 
+  scenario 'cannot create a tutorial if missing Thumbnail' do
+    admin = create(:admin)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    visit new_admin_tutorial_path
+
+    fill_in 'tutorial_title', with: "Docker Container"
+    fill_in "tutorial_description", with: "HIBBITY HOOPLA"
+    fill_in "tutorial_thumbnail", with: ""
+    click_on "Save"
+
+    expect(page).to have_content("Thumbnail can't be blank")
+  end
+
+  scenario 'cannot create a tutorial if missing description' do
+    admin = create(:admin)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    visit new_admin_tutorial_path
+
+    fill_in 'tutorial_title', with: "Docker Container"
+    fill_in "tutorial_description", with: ""
+    fill_in "tutorial_thumbnail", with: "dfssa"
+    click_on "Save"
+
+    expect(page).to have_content("Description can't be blank")
+  end
+  scenario 'cannot create a tutorial if missing Title' do
+    admin = create(:admin)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    visit new_admin_tutorial_path
+
+    fill_in 'tutorial_title', with: ""
+    fill_in "tutorial_description", with: "HIBBITY HOOPLA"
+    fill_in "tutorial_thumbnail", with: "dfas"
+    click_on "Save"
+
+    expect(page).to have_content("Title can't be blank")
+  end
 end
