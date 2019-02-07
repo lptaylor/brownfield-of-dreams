183e218b426c09fac7403930a37de4f552a55c82
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-07 15:19:15 -0700

    fix conditional for visitors

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -2,11 +2,10 @@ class TutorialsController < ApplicationController
   def show
     @tutorial = Tutorial.find(params[:id])
     @facade = TutorialFacade.new(@tutorial, params[:video_id])
-    if current_user.admin?
+    if current_user && current_user.admin?
       @facade
     else
       if @facade.current_video
-
       else
         flash[:error] = "Sorry, This tutorial is empty"
         redirect_to root_path
