1f263d58d425ae217206672eb71ba42498638dec
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-08 06:47:35 -0700

    adds amazon sms to production env and sets verified email to app mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,15 +66,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "personal_project_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-
-  config.action_mailer.default_url_options = { :host => ENV["URL"] }
-
-  config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = {
-    api_key: ENV['MAILGUN_API_KEY'],
-    domain: ENV['MAILGUN_DOMAIN'],
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'https://warm-stream-34176.herokuapp.com/' }
+  config.action_mailer.smtp_settings = {
+  :address => "email-smtp.us-west-2.amazonaws.com",
+  :port => 587,
+  :user_name => ENV["SES_SMTP_USERNAME"], #Your SMTP user
+  :password => ENV["SES_SMTP_PASSWORD"], #Your SMTP password
+  :authentication => :login,
+  :enable_starttls_auto => true
   }
-
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
