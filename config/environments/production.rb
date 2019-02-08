87e728dac7deb5d15f642801f0d64f3b94bad6a0
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-07 19:07:51 -0700

    adds mailgun gem and config to production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,17 +69,11 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => ENV["URL"] }
 
-  config.action_mailer.delivery_method = :smtp
-
- config.action_mailer.smtp_settings = {
-   address:              'smtp.sendgrid.net',
-   port:                 '587',
-   domain:               ENV["URL"],
-   user_name:            ENV["SENDGRID_USERNAME"],
-   password:             ENV["SENDGRID_PASSWORD"],
-   authentication:       'plain',
-   enable_starttls_auto: true
- }
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+    api_key: ENV['MAILGUN_API_KEY'],
+    domain: ENV['MAILGUN_DOMAIN'],
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
