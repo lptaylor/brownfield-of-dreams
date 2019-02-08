ac00e3e83cd9f214c7c61b46de465f34c0b6c7c1
Lance Taylor <lance.taylor98@hotmail.com>
2019-02-08 09:21:28 -0700

    adds testing for friends list getting nil value on friendship creation

---
+++ spec/features/user/user_sees_friends_list_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe User do
+  before(:each) do
+    VCR.use_cassette('friends') do
+      @current_user = create(:user)
+      @user_1 = create(:user, uid: 42525195, github_username: 'johnnytsunami')
+      @user = UserPresenter.new(@current_user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      visit '/dashboard'
+    end
+  end
+  it 'shows a section for friends' do
+    expect(page).to have_content('Friends List')
+  end
+
+  it 'shows friends in section' do
+    within '.github' do
+      within "#github_followers-#{@user.github_followers[0].id}" do
+        click_on 'Follow This User'
+      end
+    end
+
+    save_and_open_page
+    binding.pry
+    within '.friends' do
+      expect(page).to have_content(@user_1.github_username)
+    end
+  end
+
+end
